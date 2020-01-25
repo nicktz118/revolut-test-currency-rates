aad49f1264a1c56832619dd20d5a6c0ae57f8bf0
nicktz9 <nick.t@17.media>
2020-01-25 12:00:09 +0800

    implement NetworkStateItemViewHolder

---
+++ app/src/main/java/com/nicktz/revoluttest/utils/network/NetworkStateItemViewHolder.kt
@@ -0,0 +1,26 @@
+package com.nicktz.revoluttest.utils.network
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.nicktz.revoluttest.R
+import kotlinx.android.synthetic.main.item_network_state.view.*
+
+class NetworkStateItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bindTo(networkState: NetworkState?) = with(itemView) {
+        loading.isVisible = networkState?.status == Status.RUNNING
+        msgTextView.isVisible = networkState?.msg != null
+        msgTextView.text = networkState?.msg
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): NetworkStateItemViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_network_state, parent, false)
+            return NetworkStateItemViewHolder(view)
+        }
+    }
+}
