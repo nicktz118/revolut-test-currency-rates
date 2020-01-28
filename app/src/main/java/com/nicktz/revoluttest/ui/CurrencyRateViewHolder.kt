9f5d252ac2060fe0ee8950aef948cc53727fba49
nicktz9 <nick.t@17.media>
2020-01-28 20:42:38 +0800

    refactor logic of edittext in RateViewHolder

--- app/src/main/java/com/nicktz/revoluttest/ui/CurrencyRateViewHolder.kt
+++ app/src/main/java/com/nicktz/revoluttest/ui/CurrencyRateViewHolder.kt
@@ -1,5 +1,7 @@
 package com.nicktz.revoluttest.ui
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,58 +9,124 @@ import androidx.annotation.DrawableRes
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
-import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.nicktz.revoluttest.R
+import com.nicktz.revoluttest.utils.CurrencyFlags
+import com.nicktz.revoluttest.utils.toBigDecimalWith2digits
 import kotlinx.android.synthetic.main.item_currency_rate.view.*
 
 data class CurrencyRateDisplayItem(
-    val id: String,
     @DrawableRes val flagResId: Int?,
     val title: String,
     val subtitle: String,
-    val amount: Double?,
+    val amount: Double,
     val editable: Boolean
-)
+) {
+    companion object {
+        fun createByCurrencyRate(
+            amount: Double,
+            currency: String,
+            rate: Double
+        ): CurrencyRateDisplayItem = CurrencyRateDisplayItem(
+            flagResId = CurrencyFlags.byCurrency[currency],
+            title = currency,
+            subtitle = java.util.Currency.getInstance(currency).displayName,
+            amount = rate.times(amount),
+            editable = false
+        )
+
+        fun createBaseCurrency(
+            amount: Double,
+            currency: String
+        ): CurrencyRateDisplayItem = CurrencyRateDisplayItem(
+            flagResId = CurrencyFlags.byCurrency[currency],
+            title = currency,
+            subtitle = java.util.Currency.getInstance(currency).displayName,
+            amount = amount,
+            editable = true
+        )
+    }
+}
 
 class CurrencyRateViewHolder(
     view: View,
     private val onClickCallBack: (CurrencyRateDisplayItem) -> Unit,
-    private val onEditCallBack: (CurrencyRateDisplayItem) -> Unit
+    private val onAmountChanged: (Double) -> Unit
 ) : RecyclerView.ViewHolder(view) {
 
-    private var currencyRate : CurrencyRateDisplayItem? = null
+    private lateinit var currencyRate: CurrencyRateDisplayItem
+
+    private var textWatcher: TextWatcher = object : TextWatcher {
+
+        override fun afterTextChanged(s: Editable?) {
+            s?.let {
+                if (it.toString().isEmpty().not() || it.toString().isBlank().not()) {
+                    onAmountChanged.invoke(it.toString().toDouble())
+                } else {
+                    onAmountChanged.invoke(0.0)
+                }
+            }
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+    }
 
     init {
         view.setOnClickListener {
             currencyRate?.let { item ->
-               onClickCallBack(item)
+                onClickCallBack(item)
             }
         }
     }
 
-    fun bind(currencyRate: CurrencyRateDisplayItem) = with(itemView) {
-        titleTextView.text = currencyRate.title
-        subtitleTextView.text = currencyRate.subtitle
-        amountEditText.isEnabled = currencyRate.editable
+    fun bind(item: CurrencyRateDisplayItem) = with(itemView) {
+        itemView.amountEditText.removeTextChangedListener(textWatcher)
+        currencyRate = item
+        titleTextView.text = item.title
+        subtitleTextView.text = item.subtitle
         Glide.with(flagImageView.context)
-            .load(currencyRate.flagResId)
+            .load(item.flagResId)
             .transform(CircleCrop())
             .into(flagImageView)
+        updateAmountEditable(item)
+        updateAmount(item)
     }
 
     fun updateAmount(currencyRate: CurrencyRateDisplayItem) {
         this.currencyRate = currencyRate
-        itemView.amountEditText.setText(currencyRate.amount.toString())
+        if (itemView.amountEditText.text.isNotEmpty() &&
+            itemView.amountEditText.text.isNotBlank() && currencyRate.editable
+        ) {
+            return
+        } else if ((itemView.amountEditText.text.isEmpty() ||
+                    itemView.amountEditText.text.isBlank()) && currencyRate.editable
+        ) {
+            itemView.amountEditText.setText(currencyRate.amount.toBigDecimal().stripTrailingZeros().toPlainString())
+        } else {
+            itemView.amountEditText.setText(currencyRate.amount.toBigDecimalWith2digits().toPlainString())
+        }
+    }
+
+    fun updateAmountEditable(currencyRate: CurrencyRateDisplayItem) {
+        this.currencyRate = currencyRate
+        itemView.amountEditText.isEnabled = currencyRate.editable
+        if (currencyRate.editable) {
+            itemView.amountEditText.addTextChangedListener(textWatcher)
+        } else {
+            itemView.amountEditText.removeTextChangedListener(textWatcher)
+        }
     }
 
     companion object {
         fun create(
             parent: ViewGroup,
             onClickCallBack: (CurrencyRateDisplayItem) -> Unit,
-            onEditCallBack: (CurrencyRateDisplayItem) -> Unit): CurrencyRateViewHolder {
+            onAmountChanged: (Double) -> Unit
+        ): CurrencyRateViewHolder {
             val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.item_currency_rate, parent, false)
-            return CurrencyRateViewHolder(view, onClickCallBack, onEditCallBack)
+            return CurrencyRateViewHolder(view, onClickCallBack, onAmountChanged)
         }
     }
 }
