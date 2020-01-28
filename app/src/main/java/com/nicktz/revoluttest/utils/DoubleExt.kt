08d68b747931dcfb9e573d780df42e886d434bad
nicktz9 <nick.t@17.media>
2020-01-28 20:40:17 +0800

    implement Double extension for scaling 2 digitals

--- app/src/main/java/com/nicktz/revoluttest/utils/DoubleExt.kt
+++ app/src/main/java/com/nicktz/revoluttest/utils/DoubleExt.kt
@@ -1,2 +1,8 @@
 package com.nicktz.revoluttest.utils
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+fun Double.toBigDecimalWith2digits(): BigDecimal {
+    return BigDecimal(this).setScale(2, RoundingMode.HALF_EVEN)
+}
