a07ec540853108484fa1ac11aeba08d0d63a8340
nicktz9 <nick.t@17.media>
2020-01-28 20:43:21 +0800

    implement CurrencyRatesFramgment'sui reaction with viewmodel

--- app/src/main/java/com/nicktz/revoluttest/ui/CurrencyRatesFragment.kt
+++ app/src/main/java/com/nicktz/revoluttest/ui/CurrencyRatesFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.nicktz.revoluttest.R
+import kotlinx.android.synthetic.main.fragment_rates.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.math.BigDecimal
 
 class CurrencyRatesFragment : Fragment() {
 
@@ -16,15 +19,35 @@ class CurrencyRatesFragment : Fragment() {
 
     private val viewModel: CurrencyRatesViewModel by viewModel()
 
+    private val currencyAdapter = CurrencyRateAdapter(::onItemClicked, ::onAmountChanged)
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_rates, container, false)
-    }
+    ): View = inflater.inflate(R.layout.fragment_rates, container, false)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        currencyRatesRecyclerView.apply {
+            adapter = currencyAdapter
+        }
+        registerObservers()
     }
 
+    private fun registerObservers() {
+        viewModel.result.observe(viewLifecycleOwner, Observer { result ->
+            currencyAdapter.submitList(result)
+        })
+        viewModel.networkState.observe(viewLifecycleOwner, Observer { networkState ->
+            currencyAdapter.setNetworkState(networkState)
+        })
+    }
+
+    private fun onItemClicked(currencyRateDisplayItem: CurrencyRateDisplayItem) {
+        viewModel.onCurrencyClicked(currencyRateDisplayItem)
+    }
+
+    private fun onAmountChanged(amount: Double) {
+        viewModel.onAmountChanged(amount)
+    }
 }
